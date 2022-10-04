f22f6579ead5f8c0b9e823b9d5c54cb65dbf2c38
antoniogatfdz <101221289+antoniogatfdz@users.noreply.github.com>
2022-10-04 23:55:49 +0200

    Update index.js

--- src/components/aboutme/index.js
+++ src/components/aboutme/index.js
@@ -11,7 +11,6 @@ const About = () => {
                     <img className="palette" src={palette} alt="palette" />
                     <img className="arrow" src={arrow} alt="arrow" />
                     <p className="subtitle">Sobre mi</p>
-                    <img className="title_ilustration" src={emoji} alt="about me" />
                 </div>
             </div>
             <div className="card">
@@ -23,49 +22,33 @@ const About = () => {
                 <div className="section_subtitle">Perfil</div>
                 <code>
                     <p>
-                        Profesional del <b className="hilight">diseño gráfico </b>
-                        con más de 20 años de experiencia y <b className="hilight">
-                        desarrollador javascript fullstack MERN</b> ((M)ongoDB/(M)ySql, (E)xpress, (R)eact/next, (N)ode.)
+                        Desarrollador <b className="hilight">informático </b>
+                        con más de 2 años de experiencia y <b className="hilight">
+                        CREADOR de </b> RETA Company
                     </p>
                 </code>
-                <div className="section_subtitle">Armada Argentina</div>
+                <div className="section_subtitle">Historia</div>
                 <code>
                     <p>
-                        Desde 2005 me desempeño como
-                        <b className="hilight"> diseñador gráfico </b>
-                        en Gaceta Marinera, que pertenece al Departamento de Comunicación de la Secretaría
-                        General Naval de la Armada Argentina. Allí comencé desarrollando producciones
-                        multimediales en <b className="hilight"> Actionscript </b>
-                        y realizando diseño editorial.
+                        Desde hace unos años me adentré en el mundo de 
+                        <b className="hilight"> la programación </b>
+                        aprendiendo multiples lenguajes de programación. Como lo son <b className="hilight"> Python o JavaScript </b>
+                        que me acompañan durante mis procesos.
                     </p>
                     <p>
-                        Actualmente me encargo del diseño de las infografías para la revista "Gaceta Marinera"
-                        además de otras tareas, tales como animaciones en After Effects, diseños y desarrollos para la web 
-                        y colaboraciones para distintas áreas de la Armada.
+                        Actualmente me encuentro en desarrollo de varios proyestos, que los catalogo como <b className="hilight"> AMBICIOSOS </b>
                     </p>
                 </code>
-                <div className="section_subtitle">Dirección de Cultura</div>
+                <div className="section_subtitle">RETA Company</div>
                 <code>
-                    Entre 2003 y 2011 trabajé para la<b className="hilight"> Dirección de Cultura de Coronel Rosales </b>
-                    donde me desempeñé como diseñador y desarrollador web encargándome de la cartelera cultural online de dicha dirección (html, css, javascript, jquery). 
+                    Hace unos años decidí crear un proyecto que llevar a lo más grande: <b className="hilight"> RETA Company. </b>
+                    En el que estoy invirtiendo una gran cantidad de tiempo y dinero. 
                 </code>
                 <div className="section_subtitle">Mis comienzos...</div>
                 <code>
-                    Inicié mis estudios de diseño en 1997 y dí mis primeros pasos en el desarrollo con ActionScript allá por el año 1999. Eran días de la web 1.0.
-
+                    Mis inicios fueron dificil debido que empecé a una pronta edad con la "locura" de dedicarme a "algo" que 
+                    te puede ir bien mal o bien y que hay que arriesgarse a tomar una decisión
                 </code>
-                <div className="section_subtitle">Front, back.... fullstack?</div>
-                <code>
-                    <p>
-                        Amo tanto el <b className="hilight">frontend</b> como el <b className="hilight">backend</b>!.
-                    </p>
-                    <p>
-                        Ser diseñador y animador, sumado a las skills
-                        de desarrollo fullstack, me dan la capacidad de crear aplicaciones web de punta a punta,
-                        desde la concepción misma de la UI/UX, hasta la programación, desarrollo y deployment.
-                    </p>
-                </code>
-            </div>
         </article>
     );
 }
