77c1b6ee070d1023b6c6e1728c9edf61e66b2b8c
antoniogatfdz <101221289+antoniogatfdz@users.noreply.github.com>
2022-10-05 00:13:02 +0200

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -22,8 +22,8 @@ const Header = () => {
                         <Item img={about} title="sobre mí" route="#aboutme" />
                         <Item img={techs} title="tecnologías" route="#technologies" />
                         <Item img={portfolio} title="portfolio" route="#portfolio" />
-                        <Item img={github} title="github" route="https://github.com/ambergesio" target="_blank"/>
-                        <Item img={linkedin} title="linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
+                        <Item img={github} title="github" route="https://github.com/antoniogatfdz" target="_blank"/>
+                        <Item img={linkedin} title="linkedin" route="https://es.linkedin.com/in/antonio-gat-fern%C3%A1ndez-4600a11a1" target="_blank"/>
                         <Item img={contact} title="contacto" route="#contact" />
                     </nav>
                 </div>
@@ -35,8 +35,8 @@ const Header = () => {
                     <Item img={about} title="sobre mí" route="#aboutme" />
                     <Item img={techs} title="tecnologías" route="#technologies" />
                     <Item img={portfolio} title="portfolio" route="#portfolio" />
-                    <Item img={github} title="github" route="https://github.com/ambergesio" target="_blank"/>
-                    <Item img={linkedin} title="linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
+                    <Item img={github} title="github" route="https://github.com/antoniogatfdz" target="_blank"/>
+                    <Item img={linkedin} title="linkedin" route="https://es.linkedin.com/in/antonio-gat-fern%C3%A1ndez-4600a11a1" target="_blank"/>
                     <Item img={contact} title="contacto" route="#contact" />
                 </div>
 
@@ -44,18 +44,18 @@ const Header = () => {
                 <div className="main_title_container">
                     <div className="title_container">
                         <div className="title">
-                            <p className="name">Abel Martín</p>
-                            <p className="lastName">BERGESIO</p>
+                            <p className="name">Antonio Gat</p>
+                            <p className="lastName">Fernández</p>
                         </div>
                         <div className="drop_title">
                             <div className="drop_border">
-                                <p className="drop">Fullstack MERN / Diseñador gráfico</p>
+                                <p className="drop" href="https://retacorp.es">Developer</p>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <img className="hero_img" src={mac} alt="Abel Martín Bergesio" title="Abel Martín Bergesio" />
-                    </div>
+                    //<div>
+                        //<img className="hero_img" src={mac} alt="Abel Martín Bergesio" title="Abel Martín Bergesio" />//
+                    </div>//
                 </div>
             </section>
     );
